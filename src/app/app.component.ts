423e820634fc63b9ec2567504ae83c752c94928a
SaadS19 <121642559+SaadS19@users.noreply.github.com>
2023-12-28 17:17:30 +0500

    Angular Js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  serverElements=[
+    {type:'server',name : 'TestServer1',content:'A Test Server1'},
+];
+onServerAdded(serverData:{serverName:string,serverContent:string}) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
+}
+
+onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+  this.serverElements.push({
+    type: 'blueprint',
+    name: blueprintData.serverName,
+    content: blueprintData.serverContent
+  });
+}
 }
